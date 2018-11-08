aabda56c96ea36721bd5f601eb122986df72d6b5
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2018-11-08 15:01:50 +0100

    keep the original branch type when picking branches randomly

--- dev/sim_utils.py
+++ dev/sim_utils.py
@@ -338,7 +338,8 @@ def pick_branches(tree, pseudotime):
     """
     timezone = tree.populate_timezone()
     assignments = assign_branches(tree.branch_times(), timezone)
-    branches = np.zeros(len(pseudotime), dtype=str)
+    branch_type = type(tree.branches[0])
+    branches = np.zeros(len(pseudotime), dtype=branch_type)
     for n, t in enumerate(pseudotime):
         branches[n] = pick_branch(tree, t, timezone, assignments)
     return branches
