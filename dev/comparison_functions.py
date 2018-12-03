4423dec5265991eb8aa75628101f99cebb85e6f6
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2018-12-03 18:31:33 +0100

    reogranized plotting to not have overlaps

--- dev/comparison_functions.py
+++ dev/comparison_functions.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 def plot(real_name, real, sim, alpha=0.5):
     sim_means, sim_vars, sim_zeros_gene, sim_zeros_cell, sim_totals = sim
     real_cells, real_genes = real
-    fig, ax = plt.subplots(ncols=3, nrows=3)
-    fig.set_size_inches(12, 12)
+    fig, ax = plt.subplots(ncols=4, nrows=2)
+    fig.set_size_inches(20, 10)
 
     # mean ~ variance
-    ax[0][0].set_title("mean ~ variance")
-    ax[0][0].set_xlabel("log2(x+1) of avg. gene expr.")
-    ax[0][0].set_ylabel("log2(x+1) of gene expr. variance")
-    s1 = ax[0][0].scatter(np.log2(real_genes.loc['means']+1),
+    ax[1][0].set_title("mean ~ variance")
+    ax[1][0].set_xlabel("log2(x+1) of avg. gene expr.")
+    ax[1][0].set_ylabel("log2(x+1) of gene expr. variance")
+    s1 = ax[1][0].scatter(np.log2(real_genes.loc['means']+1),
                         np.log2(real_genes.loc['var']+1),
                         label=real_name, alpha=alpha)
-    s2 = ax[0][0].scatter(np.log2(sim_means+1),
+    s2 = ax[1][0].scatter(np.log2(sim_means+1),
                         np.log2(sim_vars+1),
                         label="prosstt", alpha=alpha)
 
     # mean gene expression
-    ax[0][1].set_title("avg. gene expr.")
-    ax[0][1].set_xlabel("log2(x+1) avg. expr. data")
-    ax[0][1].set_ylabel("log2(x+1) avg. expr. simulation")
-    ax[0][1].scatter(np.log2(real_genes.loc['means'] + 1),
+    ax[1][1].set_title("avg. gene expr.")
+    ax[1][1].set_xlabel("log2(x+1) avg. expr. data")
+    ax[1][1].set_ylabel("log2(x+1) avg. expr. simulation")
+    ax[1][1].scatter(np.log2(real_genes.loc['means'] + 1),
                      np.log2(sim_means+1), alpha=alpha)
     lims = [
-        np.min([ax[0][1].get_xlim(), ax[0][1].get_ylim()]),
-        np.max([ax[0][1].get_xlim(), ax[0][1].get_ylim()]),
+        np.min([ax[1][1].get_xlim(), ax[1][1].get_ylim()]),
+        np.max([ax[1][1].get_xlim(), ax[1][1].get_ylim()]),
     ]
-    ax[0][1].plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+    ax[1][1].plot(lims, lims, 'k-', alpha=0.75, zorder=0)
 #     b1 = ax[0][1].boxplot(data, labels=[real_name, "simulated"])
 
     # variance
@@ -39,18 +39,18 @@ def plot(real_name, real, sim, alpha=0.5):
     b1 = ax[0][2].boxplot(data, labels=[real_name, "simulated"])
 
     # percentage zeros per gene
-    ax[1][0].set_title("%zeros per gene")
-    ax[1][0].set_ylabel("percentage zeros per gene")
+    ax[0][0].set_title("%zeros per gene")
+    ax[0][0].set_ylabel("percentage zeros per gene")
     data = [(real_genes.loc['zeros'] / real_cells.shape[1]),
             (sim_zeros_gene / len(sim_zeros_cell))]
-    b1 = ax[1][0].boxplot(data, labels=[real_name, "simulated"])
+    b1 = ax[0][0].boxplot(data, labels=[real_name, "simulated"])
 
     # percentage zeros per cell
-    ax[1][1].set_title("%zeros per cell")
-    ax[1][1].set_ylabel("percentage zeros per cell")
+    ax[0][1].set_title("%zeros per cell")
+    ax[0][1].set_ylabel("percentage zeros per cell")
     data = [(real_cells.loc['zeros'] / real_genes.shape[1]),
             (sim_zeros_cell / len(sim_zeros_gene))]
-    b1 = ax[1][1].boxplot(data, labels=[real_name, "simulated"])
+    b1 = ax[0][1].boxplot(data, labels=[real_name, "simulated"])
 
     # mean-zeros relationship
     ax[1][2].set_title("mean ~ %zeros (gene)")
@@ -64,11 +64,11 @@ def plot(real_name, real, sim, alpha=0.5):
                         label="simulated", alpha=alpha)
 
     # library size
-    ax[2][0].set_title("library size")
-    ax[2][0].set_ylabel("log2(x) total UMIs per cell\nby #genes")
+    ax[0][3].set_title("library size")
+    ax[0][3].set_ylabel("log2(x) total UMIs per cell\nby #genes")
     data = [np.log2(real_cells.loc['total'] / real_genes.shape[1]),
             np.log2(sim_totals /  len(sim_zeros_gene))]
-    b1 = ax[2][0].boxplot(data, labels=[real_name, "simulated"])
+    b1 = ax[0][3].boxplot(data, labels=[real_name, "simulated"])
 
 #     # size/zero relationship
 #     ax[2][1].set_title("lib. size ~ %zeros (cell)")
