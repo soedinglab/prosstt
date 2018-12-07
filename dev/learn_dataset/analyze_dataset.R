0032996da2a66bd63567ee4bd476aaf58d11a938
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2018-12-07 19:20:43 +0100

    moved R analysis to notebook --> remove analyze_dataset.R

--- dev/learn_dataset/analyze_dataset.R
+++ dev/learn_dataset/analyze_dataset.R
@@ -14,11 +14,12 @@ time <- unlist(lapply(emb$Branches, length))
 time
 
 # now get the change in average expression:
+
+mean_exp <- list()
 for (i in 1:5) {
   b <- emb$Branches[[i]]
   emb$Nodes[b, ][emb$Nodes[b, ] <= 0] <- 1e-7
-  write.csv(x = emb$Nodes[b, ],
-            file = paste("~/Documents/repos/prosstt/dev/learn_dataset/", branch_names[i], ".csv", sep=""))
+  mean_exp[[i]] <- emb$Nodes[b, ]
 }
 
 # calculate tree density (how much % of cells each node has):
@@ -35,55 +36,47 @@ for (i in 1:5) {
 }
 
 # calculate variance parameters
-progenitor_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] %in% emb$Branches[[1]][1]]
-nonskeletal_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] %in% emb$Branches[[2]][1]]
-intermediate_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] %in% emb$Branches[[3]][1:2]]
-bone_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] %in% emb$Branches[[4]][1]]
-cartilage_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] %in% emb$Branches[[5]][1]]
+means <- array(0, c(100, dim(emb$CellCoords)[2]))
+vars <- array(0, c(100, dim(emb$CellCoords)[2]))
 
-CellCoordinates <- tree$CellCoords[,1:2]
-plot(CellCoordinates, pch=16, cex=1.5)
-points(CellCoordinates[progenitor_cells, ], col="red", pch=16, cex=1.2)
-points(CellCoordinates[nonskeletal_cells, ], col="cyan", pch=16, cex=1.2)
-points(CellCoordinates[intermediate_cells, ], col="green", pch=16, cex=1.2)
-points(CellCoordinates[bone_cells, ], col="orange", pch=16, cex=1.2)
-points(CellCoordinates[cartilage_cells, ], col="yellow", pch=16, cex=1.2)
+for(n in 1:100) {
+  node_cells <- emb$Cells2TreeNodes[,1][emb$Cells2TreeNodes[,2] == n]
+  means[n,] <- apply(emb$CellCoords[node_cells, ], 2, mean)
+  vars[n,] <- apply(emb$CellCoords[node_cells, ], 2, var)
+}
 
-progenitor_means <- apply(emb$CellCoords[progenitor_cells, ], 2, mean)
-nonskeletal_means <- apply(emb$CellCoords[nonskeletal_cells, ], 2, mean)
-intermediate_means <- apply(emb$CellCoords[intermediate_cells, ], 2, mean)
-bone_means <- apply(emb$CellCoords[bone_cells, ], 2, mean)
-cartilage_means <- apply(emb$CellCoords[cartilage_cells, ], 2, mean)
+genes <- dim(emb$CellCoords)[2]
+alphas <- rep(0, genes)
+betas <- rep(0, genes)
 
-means <- rbind(progenitor_means, nonskeletal_means, intermediate_means, bone_means, cartilage_means)
-rownames(means) <- NULL
-
-progenitor_var <- apply(emb$CellCoords[progenitor_cells, ], 2, var)
-nonskeletal_var <- apply(emb$CellCoords[nonskeletal_cells, ], 2, var)
-intermediate_var <- apply(emb$CellCoords[intermediate_cells, ], 2, var)
-bone_var <- apply(emb$CellCoords[bone_cells, ], 2, var)
-cartilage_var <- apply(emb$CellCoords[cartilage_cells, ], 2, var)
-
-vars <- rbind(progenitor_var, nonskeletal_var, intermediate_var, bone_var, cartilage_var)
-rownames(vars) <- NULL
-
-G <- dim(emb$CellCoords)[2]
-alphas <- rep(0, G)
-betas <- rep(0, G)
-
-for (g in 1:G) {
+for (g in 1:genes) {
   m <- means[, g]
   s <- vars[, g]
   A <- cbind(rep(0, length(m)), m, m^2)
-  b <- s
+  b <- s #rep(1, length(m))
   G <- matrix(nrow=2, ncol=3, byrow = TRUE, data = c(0, 0, 1, 0, 1, 0))
   h <- c(1e-7, 1+1e-7)
-  constrained_model <- lsei(A = A, B = b, G = G, H = h, type=2)
+  constrained_model <- tryCatch(
+    {
+      lsei(A = A, B = b, G = G, H = h, type=2)
+    },
+    error=function(cond) {
+      message(cond)
+      message("\nrescaling...")
+      m <- means[, g] / 1000
+      s <- vars[, g] / 1000
+      A <- cbind(rep(0, length(m)), m, m^2)
+      b <- s #rep(1, length(m))
+      lsei(A = A, B = b, G = G, H = h, type=2)
+    },
+    finally={}
+  )
+  
   alphas[g] <- constrained_model$X[3]
   betas[g] <- constrained_model$X[2]
 }
 
-g <- 291
+g <- 121
 plot(means[,g], vars[,g])
 x <- seq(min(means[,g]), max(means[,g]), 0.1)
 points(x, alphas[g]*x^2+betas[g]*x, type="l", col="blue")
